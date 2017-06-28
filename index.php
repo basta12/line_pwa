987aa44a9da8da2851a27ccb711d2cc12bc46723
basta12 <I3asta@windowslive.com>
2017-06-28 10:35:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
 $access_token = '72R8yvYS3LrDrchnoN5IKWCYg6K8MKylUDE9lLBwlgqgaSuXAc9HAelnUdsJJgdGE+TZg38Cgt5EjYNGKWHzUou0nUlss8A4e+kPuGfBtgD9Nh+5NFkX/WMAVYe2b2idvXDvo5oabXsaLBiALNbBQwdB04t89/1O/w1cDnyilFU=';
 
+$url = "https://lh4.ggpht.com/An8v98KQi85Obs2-240UGSPeIyyq9mOwRvxbXzxMvvqloIOmUN3scTVN9nX3-q128Eg=h900";
+	$web_page = file_get_contents( str_replace("##query##",urlencode($k), $url ));
+ 
+	$tieni = stristr($web_page,"dyn.setResults(");
+	$tieni = str_replace( "dyn.setResults(","", str_replace(stristr($tieni,");"),"",$tieni) );
+	$tieni = str_replace("[]","",$tieni);
+	$m = preg_split("/[\[\]]/",$tieni);
+	$x = array();
+	for($i=0;$i<count($m);$i++) {
+		$m[$i] = str_replace("/imgres?imgurl\\x3d","",$m[$i]);
+		$m[$i] = str_replace(stristr($m[$i],"\\x26imgrefurl"),"",$m[$i]);
+		$m[$i] = preg_replace("/^\"/i","",$m[$i]);
+		$m[$i] = preg_replace("/^,/i","",$m[$i]);
+		if ($m[$i]!="") array_push($x,$m[$i]);
+	}
+	return $x;
+
+
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -46,23 +64,6 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-getGoogleImg();
 
-function getGoogleImg($k) {
-	$url = "https://lh4.ggpht.com/An8v98KQi85Obs2-240UGSPeIyyq9mOwRvxbXzxMvvqloIOmUN3scTVN9nX3-q128Eg=h900";
-	$web_page = file_get_contents( str_replace("##query##",urlencode($k), $url ));
- 
-	$tieni = stristr($web_page,"dyn.setResults(");
-	$tieni = str_replace( "dyn.setResults(","", str_replace(stristr($tieni,");"),"",$tieni) );
-	$tieni = str_replace("[]","",$tieni);
-	$m = preg_split("/[\[\]]/",$tieni);
-	$x = array();
-	for($i=0;$i<count($m);$i++) {
-		$m[$i] = str_replace("/imgres?imgurl\\x3d","",$m[$i]);
-		$m[$i] = str_replace(stristr($m[$i],"\\x26imgrefurl"),"",$m[$i]);
-		$m[$i] = preg_replace("/^\"/i","",$m[$i]);
-		$m[$i] = preg_replace("/^,/i","",$m[$i]);
-		if ($m[$i]!="") array_push($x,$m[$i]);
-	}
-	return $x;
-}
+	
+
