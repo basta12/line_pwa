7652b3806de0ba1fe5efdd80cf5f27e01dc1a602
basta12 <I3asta@windowslive.com>
2017-06-28 09:19:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			
-			if($text == 'ขอดูรูป'){
+			if($text == 'm'){
 				$text = $event['message']['text'];
 				$replyToken = $event['replyToken'];
 				$messages = [
