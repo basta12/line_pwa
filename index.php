94738c636fbb2d0000afc46ed0a1474721eb2f3a
basta12 <I3asta@windowslive.com>
2017-06-28 09:17:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,24 +11,26 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => 'ok'
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
+			
+			if($text == 'ขอดูรูป'){
+				$text = $event['message']['text'];
+				$replyToken = $event['replyToken'];
+				$messages = [
+					'type' => 'text',
+					'text' => 'ok'
+				];
+				$url = 'https://api.line.me/v2/bot/message/reply';
+				
+				$data = [
+					'replyToken' => $replyToken,
+					'messages' => [$messages],
+				];
+				
+				$post = json_encode($data);
+			}
+			
+		
+		
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
