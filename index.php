a05aa1bc8619bb2b766d26f5042ddfb7933ef7a3
basta12 <I3asta@windowslive.com>
2017-06-28 10:33:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -46,4 +46,23 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK";
+getGoogleImg();
+
+function getGoogleImg($k) {
+	$url = "https://lh4.ggpht.com/An8v98KQi85Obs2-240UGSPeIyyq9mOwRvxbXzxMvvqloIOmUN3scTVN9nX3-q128Eg=h900";
+	$web_page = file_get_contents( str_replace("##query##",urlencode($k), $url ));
+ 
+	$tieni = stristr($web_page,"dyn.setResults(");
+	$tieni = str_replace( "dyn.setResults(","", str_replace(stristr($tieni,");"),"",$tieni) );
+	$tieni = str_replace("[]","",$tieni);
+	$m = preg_split("/[\[\]]/",$tieni);
+	$x = array();
+	for($i=0;$i<count($m);$i++) {
+		$m[$i] = str_replace("/imgres?imgurl\\x3d","",$m[$i]);
+		$m[$i] = str_replace(stristr($m[$i],"\\x26imgrefurl"),"",$m[$i]);
+		$m[$i] = preg_replace("/^\"/i","",$m[$i]);
+		$m[$i] = preg_replace("/^,/i","",$m[$i]);
+		if ($m[$i]!="") array_push($x,$m[$i]);
+	}
+	return $x;
+}
