d05d1ba97ac3c91b0270e89149ee381018b61119
basta12 <I3asta@windowslive.com>
2017-06-28 09:20:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,9 +11,8 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			
-			if($text == 'm'){
-				$text = $event['message']['text'];
+			$text = $event['message']['text'];
+			if($text == 'ขอดูรูป'){				
 				$replyToken = $event['replyToken'];
 				$messages = [
 					'type' => 'text',
